5f2c880b3d8d1ee7576f7940af32aff43e6df8e6
Thomas Westermann <thomas.westermann@orange.fr>
2015-01-12 14:00:44 +0100

    delete deconnexion.php

--- Mandat/deconnexion.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-session_destroy();
-include(dirname(__FILE__).'/includes/identifiants.php');
-include(dirname(__FILE__).'/includes/header.php');
-include(dirname(__FILE__).'/includes/menu.php');
-
