27c656e9d25d23df48450d6539cf31d899d54338
Thomas Westermann <thomas.westermann@orange.fr>
2015-01-13 13:53:59 +0100

    Delete Problems

--- Mandat/Controleur/Routeur.php
+++ Mandat/Controleur/Routeur.php
@@ -18,56 +18,60 @@ class Routeur {
   // Traite une requête entrante
   public function routerRequete() {
     if (isset($_GET['action'])) {
+      if(isset($_SESSION['droit'])){
+        if($_SESSION['droit'] == 3){
+          if ($_GET['action'] == 'ajoutEtudiant') {
+            $this->ctrlAdmin->ajoutEtudiant(array($_POST['nom'], $_POST['prenom'], $_POST['identite_payeur']));
+            header('Location: index.php?action=Admin&tab=etudiant'); 
+          }
+          elseif ($_GET['action'] == 'modifierEtudiant') {
+            $this->ctrlAdmin->modifierEtudiant(array($_POST['nom'], $_POST['prenom'], $_POST['identite_payeur'], $_POST['id']));
+            header('Location: index.php?action=Admin&tab=etudiant');
+          }
+          elseif ($_GET['action'] == 'supprimerEtudiant') {
+            $this->ctrlAdmin->supprimerEtudiant(array($_GET['id']));
+            header('Location: index.php?action=Admin&tab=etudiant'); 
+          }
+          elseif ($_GET['action'] == 'ajoutPays') {
+            $this->ctrlAdmin->ajoutPays(array($_POST['code_pays'],$_POST['alpha2'],$_POST['alpha3'],$_POST['nom_en_gb'],$_POST['nom_fr_fr']));
+            header('Location: index.php?action=Admin&tab=pays'); 
+          }
+          elseif ($_GET['action'] == 'modifierPays') {
+            $this->ctrlAdmin->modifierPays(array($_POST['code_pays'],$_POST['alpha2'],$_POST['alpha3'],$_POST['nom_en_gb'],$_POST['nom_fr_fr'],$_POST['id']));
+            header('Location: index.php?action=Admin&tab=pays');
+          }
+          elseif ($_GET['action'] == 'supprimerPays') {
+            $this->ctrlAdmin->supprimerPays(array($_GET['id']));
+            header('Location: index.php?action=Admin&tab=pays');
+          }
+          elseif ($_GET['action'] == 'ajoutDroit') {
+            $this->ctrlAdmin->ajoutDroit(array($_POST['id_ent'],$_POST['code_droit'],$_POST['mdp']));
+            header('Location: index.php?action=Admin&tab=droit');
+          }
+          elseif ($_GET['action'] == 'modifierDroit') {
+            $this->ctrlAdmin->modifierDroit(array($_POST['id_ent'],$_POST['code_droit'],$_POST['mdp'],$_POST['id']));
+            header('Location: index.php?action=Admin&tab=droit');
+          }
+          elseif ($_GET['action'] == 'supprimerDroit') {
+            $this->ctrlAdmin->supprimerDroit(array($_GET['id']));
+            header('Location: index.php?action=Admin&tab=droit');
+          }
+          elseif ($_GET['action'] == 'ajoutDiplome') {
+            $this->ctrlAdmin->ajoutDiplome(array($_POST['libele_diplome']));
+            header('Location: index.php?action=Admin&tab=diplome');
+          }
+          elseif ($_GET['action'] == 'modifierDiplome') {
+            $this->ctrlAdmin->modifierDiplome(array($_POST['libele_diplome'],$_POST['id']));
+            header('Location: index.php?action=Admin&tab=diplome');
+          }
+          elseif ($_GET['action'] == 'supprimerDiplome') {
+            $this->ctrlAdmin->supprimerDiplome(array($_GET['id']));
+            header('Location: index.php?action=Admin&tab=diplome');
+          }
+        }
+      }
       if ($_GET['action'] == 'Admin') {
         $this->ctrlAdmin->admin();
-      }
-      elseif ($_GET['action'] == 'ajoutEtudiant') {
-        $this->ctrlAdmin->ajoutEtudiant(array($_POST['nom'], $_POST['prenom'], $_POST['identite_payeur']));
-        header('Location: index.php?action=Admin&tab=etudiant'); 
-      }
-      elseif ($_GET['action'] == 'modifierEtudiant') {
-        $this->ctrlAdmin->modifierEtudiant(array($_POST['nom'], $_POST['prenom'], $_POST['identite_payeur'], $_POST['id']));
-        header('Location: index.php?action=Admin&tab=etudiant'); 
-      }
-      elseif ($_GET['action'] == 'supprimerEtudiant') {
-        $this->ctrlAdmin->supprimerEtudiant(array($_GET['id']));
-        header('Location: index.php?action=Admin&tab=etudiant'); 
-      }
-      elseif ($_GET['action'] == 'ajoutPays') {
-        $this->ctrlAdmin->ajoutPays(array($_POST['code_pays'],$_POST['alpha2'],$_POST['alpha3'],$_POST['nom_en_gb'],$_POST['nom_fr_fr']));
-        header('Location: index.php?action=Admin&tab=pays'); 
-      }
-      elseif ($_GET['action'] == 'modifierPays') {
-        $this->ctrlAdmin->modifierPays(array($_POST['code_pays'],$_POST['alpha2'],$_POST['alpha3'],$_POST['nom_en_gb'],$_POST['nom_fr_fr'],$_POST['id']));
-        header('Location: index.php?action=Admin&tab=pays');
-      }
-      elseif ($_GET['action'] == 'supprimerPays') {
-        $this->ctrlAdmin->supprimerPays(array($_GET['id']));
-        header('Location: index.php?action=Admin&tab=pays');
-      }
-      elseif ($_GET['action'] == 'ajoutDroit') {
-        $this->ctrlAdmin->ajoutDroit(array($_POST['id_ent'],$_POST['code_droit'],$_POST['mdp']));
-        header('Location: index.php?action=Admin&tab=droit');
-      }
-      elseif ($_GET['action'] == 'modifierDroit') {
-        $this->ctrlAdmin->modifierDroit(array($_POST['id_ent'],$_POST['code_droit'],$_POST['mdp'],$_POST['id']));
-        header('Location: index.php?action=Admin&tab=droit');
-      }
-      elseif ($_GET['action'] == 'supprimerDroit') {
-        $this->ctrlAdmin->supprimerDroit(array($_GET['id']));
-        header('Location: index.php?action=Admin&tab=droit');
-      }
-      elseif ($_GET['action'] == 'ajoutDiplome') {
-        $this->ctrlAdmin->ajoutDiplome(array($_POST['libele_diplome']));
-        header('Location: index.php?action=Admin&tab=diplome');
-      }
-      elseif ($_GET['action'] == 'modifierDiplome') {
-        $this->ctrlAdmin->modifierDiplome(array($_POST['libele_diplome'],$_POST['id']));
-        header('Location: index.php?action=Admin&tab=diplome');
-      }
-      elseif ($_GET['action'] == 'supprimerDiplome') {
-        $this->ctrlAdmin->supprimerDiplome(array($_GET['id']));
-        header('Location: index.php?action=Admin&tab=diplome');
       }
       elseif ($_GET['action'] == 'Gestion') {
         echo "La page de gestion n'est pas implementer";
@@ -80,7 +84,7 @@ class Routeur {
       }
       elseif ($_GET['action'] == 'Connexion1') {
         $res = $this->ctrlConnec->seConnecter($_POST['pseudo'],$_POST['password']);
-          $this->ctrlConnec->connecM($res);
+        $this->ctrlConnec->connecM($res);
       }
       elseif ($_GET['action'] == 'Deconnexion') {
         $this->ctrlConnec->seDeconnecter();
